6b9e3b8f11448391894c8d4368b14b505963fa17
lnowakow@uwo.ca <lnowakow@uwo.ca>
2024-01-17 19:21:45 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# ZABER Motion Library
+
+This is the README file for the ZABER Motion Library.
+
+## Conda Environment
+
+To set up the conda environment for this project, follow these steps:
+
+1. Create a new conda environment:
+    ```bash
+    conda create -n zaber python=3.12.1
+    ```
+
+2. Activate the conda environment:
+    ```bash
+    conda activate zaber
+    ```
+
+3. Add conda-forge channel
+    ```bash
+    conda config --add channels conda-forge
+    ```
+
+4. Install zaber-motion package
+    ```bash
+    conda install zaber-motion
+    ```
+
+(NOT YET)
+3. Install the required packages:
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+4. Run the project:
+    ```bash
+    python test.py
+    ```
+
+Enjoy using the ZABER Motion Library!
---
+++ test.py
@@ -0,0 +1,70 @@
+from zaber_motion.binary import Connection, CommandCode
+from zaber_motion import Units
+import time
+
+class ZaberDevice:
+    def __init__ (port: str = "/dev/ttyUSB0"):
+        self.port = port
+        self.connection = None
+        self.device = None
+
+        setup()
+
+    def setup(self):
+        if "/dev/ttyUSB" not in self.port:
+            raise ValueError("Invalid port. Must be of the form /dev/ttyUSBx where x is a number.")
+        
+        # Open the connection.
+        self.connection = Connection.open_serial_port(self.port)
+        device_list = connection.detect_devices()
+        print("Detected {} devices".format(len(device_list)))
+        self.device = device_list[0]
+
+        # set minimum and maximum positions
+        self.device.set_limit_min(0)
+        self.device.set_limit_max(100)
+
+
+
+
+
+with Connection.open_serial_port("/dev/ttyUSB0") as connection:
+    # device_list = connection.detect_devices()
+    # print("Detected {} devices".format(len(device_list)))
+    # device = device_list[0]
+    # device.home()
+    # # get device number
+    # print(device.get_device_number())
+    devices = connection.detect_devices()
+    print("Detected {} devices".format(len(devices)))
+    device = devices[0]
+
+    # print out device info
+    print(f"Device ID: {device.device_id}")
+    print(f"Device Type: {device.device_type}")
+    print(f"Device Name: {device.name}")
+    print(f"Device Identity: {device.identity}")
+    print(f"Device Firmware Version: {device.firmware_version}")
+    print(f"Device Serial Number: {device.serial_number}")
+    print(f"Device Settings: {device.settings}")
+
+    # Send home
+    device.generic_command_no_response(CommandCode.HOME)
+    while device.is_busy():
+        time.sleep(0.1)
+
+    device.move_velocity(4, Units.VELOCITY_MILLIMETRES_PER_SECOND)
+    start = time.time()
+    while (time.time() - start) < 2:
+        print(f"Current position: {device.get_position(Units.LENGTH_MILLIMETRES)}", end="\r")
+        time.sleep(0.01)
+    print(f"Current position: {device.get_position(Units.LENGTH_MILLIMETRES)}")
+
+    device.move_velocity(-4, Units.VELOCITY_MILLIMETRES_PER_SECOND)
+    start = time.time()
+    while (time.time() - start) < 2:
+        print(f"Current position: {device.get_position(Units.LENGTH_MILLIMETRES)}", end="\r")
+        time.sleep(0.01)
+    print(f"Current position: {device.get_position(Units.LENGTH_MILLIMETRES)}")
+
+    device.stop()
